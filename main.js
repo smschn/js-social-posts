e3d95c9d0f8d37a7c9235014790de9a0f3f81b46
Luca Somaschini <somas99@gmail.com>
2022-07-20 15:43:53 +0200

    Generate posts with js (still all the same)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 const posts = [
+
     {
         "id": 1,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -10,6 +11,7 @@ const posts = [
         "likes": 80,
         "created": "2021-06-25"
     },
+
     {
         "id": 2,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -21,6 +23,7 @@ const posts = [
         "likes": 120,
         "created": "2021-09-03"
     },
+
     {
         "id": 3,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -32,6 +35,7 @@ const posts = [
         "likes": 78,
         "created": "2021-05-15"
     },
+
     {
         "id": 4,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -43,6 +47,7 @@ const posts = [
         "likes": 56,
         "created": "2021-04-03"
     },
+
     {
         "id": 5,
         "content": "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
@@ -54,4 +59,51 @@ const posts = [
         "likes": 95,
         "created": "2021-03-05"
     }
+
 ];
+
+
+
+// --- inizio milestone 1
+
+
+
+// richiamo il container
+const containerDom = document.getElementById('container');
+
+// attraverso un ciclo, creo i post
+for (let x = 0; x < posts.length; x++) {
+
+    containerDom.innerHTML += `<div class="post">
+                                <div class="post__header">
+                                    <div class="post-meta">                    
+                                        <div class="post-meta__icon">
+                                            <img class="profile-pic" src="https://unsplash.it/300/300?image=15" alt="Phil Mangione">                    
+                                        </div>
+                                        <div class="post-meta__data">
+                                            <div class="post-meta__author">Phil Mangione</div>
+                                            <div class="post-meta__time">4 mesi fa</div>
+                                        </div>                    
+                                    </div>
+                                </div>
+                                <div class="post__text">Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.</div>
+                                <div class="post__image">
+                                    <img src="https://unsplash.it/600/300?image=171" alt="">
+                                </div>
+                                <div class="post__footer">
+                                    <div class="likes js-likes">
+                                        <div class="likes__cta">
+                                            <a class="like-button  js-like-button" href="#" data-postid="1">
+                                                <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                                                <span class="like-button__label">Mi Piace</span>
+                                            </a>
+                                        </div>
+                                        <div class="likes__counter">
+                                            Piace a <b id="like-counter-1" class="js-likes-counter">80</b> persone
+                                        </div>
+                                    </div> 
+                                </div>            
+                            </div>`
+
+
+}
